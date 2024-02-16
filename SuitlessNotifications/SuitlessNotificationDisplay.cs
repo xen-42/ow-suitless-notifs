d5ba85a54f46c313832cd1ec775ec4e8159272f2
Nick <nicholasconnors@hotmail.ca>
2024-02-15 23:27:22 -0500

    This was a complete waste of time

---
+++ SuitlessNotifications/SuitlessNotificationDisplay.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace SuitlessNotifications;
+
+// Modified version of SuitNotificationDisplay
+public class SuitlessNotificationDisplay : NotificationDisplayTextLayout
+{
+	public override void Awake()
+	{
+		base.Awake();
+		this._notificationTargetType = NotificationTarget.Player;
+	}
+
+	public override void Start()
+	{
+		base.Start();
+		this._playerAudioController = Locator.GetPlayerAudioController();
+	}
+
+	public override void ExpandPool()
+	{
+		int num = this._textItemPool.Count * 2;
+		while (this._textItemPool.Count < num)
+		{
+			GameObject gameObject = GameObject.Instantiate<GameObject>(this._textDisplayTemplate, this._textDisplayRoot);
+			this._textItemPool.Add(gameObject);
+			gameObject.SetActive(false);
+		}
+	}
+
+	public override void PlayNotificationAudio()
+	{
+		this._playerAudioController.PlayNotificationTextScrolling();
+	}
+
+	public override void StopNotificationAudio()
+	{
+		if (this._playerAudioController != null)
+		{
+			this._playerAudioController.StopNotificationTextScrolling();
+		}
+	}
+
+	private PlayerAudioController _playerAudioController;
+}
--- SuitlessNotifications/SuitlessNotifications.cs
+++ SuitlessNotifications/SuitlessNotifications.cs
@@ -1,30 +1,100 @@
 ﻿using OWML.Common;
 using OWML.ModHelper;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace SuitlessNotifications
 {
     public class SuitlessNotifications : ModBehaviour
     {
-        private void Awake()
-        {
-            // You won't be able to access OWML's mod helper in Awake.
-            // So you probably don't want to do anything here.
-            // Use Start() instead.
-        }
+
 
         private void Start()
         {
-            // Starting here, you'll have access to OWML's mod helper.
-            ModHelper.Console.WriteLine($"My mod {nameof(SuitlessNotifications)} is loaded!", MessageType.Success);
-
-            // Example of accessing game code.
             LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
             {
                 if (loadScene != OWScene.SolarSystem) return;
-                var playerBody = FindObjectOfType<PlayerBody>();
-                ModHelper.Console.WriteLine($"Found player body, and it's called {playerBody.name}!",
-                    MessageType.Success);
-            };
+				OnLoadSolarSystem();
+			};
         }
+
+		private void OnLoadSolarSystem()
+		{
+			var suitedDisplayUICanvas = GameObject.FindObjectOfType<SuitNotificationDisplay>().transform.parent.gameObject;
+
+			// Instantiate inactive
+			suitedDisplayUICanvas.SetActive(false);
+			var suitlessDisplayUICanvas = GameObject.Instantiate(suitedDisplayUICanvas);
+			suitedDisplayUICanvas.SetActive(true);
+
+			// Remove all children but SuitNotificationDisplay
+			foreach (Transform child in suitlessDisplayUICanvas.transform)
+			{
+				if (child.GetComponent<SuitNotificationDisplay>() == null)
+				{
+					GameObject.Destroy(child.gameObject);
+				}
+			}
+
+			// Original is on HelmetOnUI
+			suitlessDisplayUICanvas.transform.parent = GameObject.Find("PlayerHUD/HelmetOffUI").transform;
+			suitlessDisplayUICanvas.transform.localPosition = suitedDisplayUICanvas.transform.localPosition;
+			suitlessDisplayUICanvas.transform.localRotation = Quaternion.identity;
+
+			var copiedSuitDisplay = suitlessDisplayUICanvas.GetComponentInChildren<SuitNotificationDisplay>();
+			var suitlessDisplay = copiedSuitDisplay.gameObject.AddComponent<SuitlessNotificationDisplay>();
+
+			// Copy serialized fields
+			// NotificationDisplayTextLayout
+			suitlessDisplay._textDisplayTemplate = copiedSuitDisplay._textDisplayTemplate;
+			suitlessDisplay._backgroundImage = copiedSuitDisplay._backgroundImage;
+			suitlessDisplay._displaySpace = copiedSuitDisplay._displaySpace;
+
+			// NotificationDisplay
+			suitlessDisplay._displayText = copiedSuitDisplay._displayText;
+			suitlessDisplay._displayCanvas = copiedSuitDisplay._displayCanvas;
+
+			GameObject.Destroy(copiedSuitDisplay);
+
+			SetLayersRecursively(suitlessDisplayUICanvas, LayerMask.NameToLayer("UI"));
+
+			// Create suitless hud camera
+			/*
+			var suitlessHUDCameraObj = new GameObject("SuitlessHUDCamera");
+			suitlessHUDCameraObj.transform.parent = suitlessDisplayUICanvas.transform.parent;
+			suitlessHUDCameraObj.transform.localPosition = Vector3.zero;
+			suitlessHUDCameraObj.transform.localRotation = Quaternion.identity;
+			var suitlessHUDCamera = suitlessHUDCameraObj.AddComponent<Camera>();
+			suitlessHUDCameraObj.AddComponent<SuitlessHUDCamera>();
+			*/
+
+			foreach (var canvas in suitlessDisplayUICanvas.GetComponentsInChildren<Canvas>(true).Append(suitlessDisplayUICanvas.GetComponent<Canvas>()))
+			{
+				ModHelper.Events.Unity.FireOnNextUpdate(() => canvas.worldCamera = Locator.GetPlayerCamera().mainCamera);
+				//canvas.worldCamera = suitlessHUDCamera;
+			}
+
+			suitlessDisplayUICanvas.SetActive(true);
+		}
+
+		private void SetLayersRecursively(GameObject obj, LayerMask uiLayer)
+		{
+			foreach (Transform child in obj.GetComponentInChildren<Transform>(true))
+			{
+				child.gameObject.layer = uiLayer;
+			}
+			obj.layer = uiLayer;
+		}
+
+#if DEBUG
+		public void Update()
+		{
+			if (Keyboard.current[Key.Numpad7].wasReleasedThisFrame)
+			{
+				NotificationManager.SharedInstance.PostNotification(new NotificationData(NotificationTarget.All, "TEST", 7f, true), false);
+			}
+		}
     }
+#endif
 }
